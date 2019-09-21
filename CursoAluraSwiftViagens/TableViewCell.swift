c0a1658265e1282543f5651fa8ed9efb22587602
rodrigopaixaoMac <rodrigo.de.f.paixao@gmail.com>
2019-09-21 15:33:41 -0300

    Finalizando aula 2

---
+++ CursoAluraSwiftViagens/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  CursoAluraSwiftViagens
+//
+//  Created by Rodrigo Paixão on 21/09/19.
+//  Copyright © 2019 Rodrigo Paixão. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CursoAluraSwiftViagens/Viagem.swift
@@ -0,0 +1,9 @@
+//
+//  Viagem.swift
+//  CursoAluraSwiftViagens
+//
+//  Created by Rodrigo Paixão on 21/09/19.
+//  Copyright © 2019 Rodrigo Paixão. All rights reserved.
+//
+
+import Foundation
