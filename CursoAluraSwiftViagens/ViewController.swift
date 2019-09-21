4ec88ad0649c102c54c58af6721ac137c7722fbd
rodrigopaixaoMac <rodrigo.de.f.paixao@gmail.com>
2019-09-21 15:33:53 -0300

    Finalizando aula 2

--- CursoAluraSwiftViagens/ViewController.swift
+++ CursoAluraSwiftViagens/ViewController.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource  {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tabelaViagens: UITableView!
+    @IBOutlet weak var viewPacotes: UIView!
+    @IBOutlet weak var viewHoteis: UIView!
     
-    let listaViagens:Array<String> = ["Rio de Janeiro", "Ceara", "São Paulo"]
+    let listaViagens:Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabelaViagens.dataSource = self
-        // Do any additional setup after loading the view.
+        self.tabelaViagens.delegate = self
+        self.viewPacotes.layer.cornerRadius = 10
+        self.viewHoteis.layer.cornerRadius = 10
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -25,10 +29,20 @@ class ViewController: UIViewController, UITableViewDataSource  {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = listaViagens[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        
+        let viagemAtual = listaViagens[indexPath.row]
+        cell.labelTitulo.text = viagemAtual.titulo
+        cell.labelQuantidadeDias.text = "\(viagemAtual.quantidadeDeDias) dias"
+        cell.labelPreco.text = viagemAtual.preco
+        cell.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 175
+    }
+    
 }
 
