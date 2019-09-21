8e6116f07c3fb0b0684aa8a07543e40ff98270a8
rodrigopaixaoMac <rodrigo.de.f.paixao@gmail.com>
2019-09-21 10:38:57 -0300

    Initial Commit

---
+++ CursoAluraSwiftViagens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CursoAluraSwiftViagens
+//
+//  Created by Rodrigo Paixão on 21/09/19.
+//  Copyright © 2019 Rodrigo Paixão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
