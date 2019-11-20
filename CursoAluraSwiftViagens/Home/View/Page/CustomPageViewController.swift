97e3de5289bf917b2694f727a515713879001170
rodrigopaixaoMac <rodrigo.de.f.paixao@gmail.com>
2019-11-20 19:39:18 -0300

    Inserindo PageViewController

---
+++ CursoAluraSwiftViagens/Home/View/Page/CustomPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomPageViewController.swift
+//  CursoAluraSwiftViagens
+//
+//  Created by Rodrigo Paixão on 20/11/19.
+//  Copyright © 2019 Rodrigo Paixão. All rights reserved.
+//
+
+import UIKit
+
+class CustomPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
