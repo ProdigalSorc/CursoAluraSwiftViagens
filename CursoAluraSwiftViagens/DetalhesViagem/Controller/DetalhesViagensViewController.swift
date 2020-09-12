a9ea211d0152c385e2b45941cb17394c28806299
rodrigopaixaoMac <rodrigo.de.f.paixao@gmail.com>
2020-09-12 11:52:43 -0300

    Adicionando ScrollView q n funciona

--- CursoAluraSwiftViagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
+++ CursoAluraSwiftViagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
@@ -13,13 +13,15 @@ class DetalhesViagensViewController: UIViewController {
     @IBOutlet weak var imagemPacoteViagem: UIImageView!
     @IBOutlet weak var labelTituloPacoteViagem: UILabel!
     @IBOutlet weak var labelDescricaoPacoteViagem: UILabel!
-    
+    @IBOutlet weak var scrollPrincipal: UIScrollView!
     @IBOutlet weak var labelDataViagem: UILabel!
     @IBOutlet weak var labelPrecoPacoteViagem: UILabel!
     var pacoteSelecionado:PacoteViagem?
+    @IBOutlet weak var textFieldData: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(aumentaScroll(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         if let pacote = pacoteSelecionado {
             self.imagemPacoteViagem.image = UIImage(named: pacote.viagem.caminhoDaImagem)
             self.labelTituloPacoteViagem.text = pacote.viagem.titulo
@@ -27,6 +29,23 @@ class DetalhesViagensViewController: UIViewController {
             self.labelDataViagem.text = pacote.dataViagem
             self.labelPrecoPacoteViagem.text = pacote.viagem.preco
         }
+    }
+    
+    @objc func aumentaScroll(notification:Notification){
+        self.scrollPrincipal.contentSize = CGSize(width: self.scrollPrincipal.frame.width, height: self.scrollPrincipal.frame.height + 320)
+    }
+    @IBAction func textFieldEntrouFoco(_ sender: UITextField) {
+        print("classeSistema")
+        let datePickerView = UIDatePicker()
+        datePickerView.datePickerMode = .date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: #selector(exibeDataTextFiel(sender:)), for: .valueChanged)
+    }
+    
+    @objc func exibeDataTextFiel(sender: UIDatePicker){
+        let formatador = DateFormatter()
+        formatador.dateFormat = "dd MM yyyy"
+        self.textFieldData.text = formatador.string(from: sender.date)
     }
     
     @IBAction func botaoVolar(_ sender: UIButton) {
